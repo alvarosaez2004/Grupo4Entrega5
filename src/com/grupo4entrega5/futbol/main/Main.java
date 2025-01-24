80d973582421a4f6e4e513f53822865fab25bdcb
DESKTOP-I8BIFCC\saeza <saez.alvaro2004@gmail.com>
2025-01-24 14:40:16 +0100

    main

--- src/com/grupo4entrega5/futbol/main/Main.java
+++ src/com/grupo4entrega5/futbol/main/Main.java
@@ -1,10 +1,12 @@
 package com.grupo4entrega5.futbol.main;
+
 import com.grupo4entrega5.futbol.Fichajes.TraspasoJugadores;
 import com.grupo4entrega5.futbol.alineacion.Posiciones;
 import com.grupo4entrega5.futbol.equipo.Jugadores;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-       Jugadores jugador1 = new Jugadores("Vinicius", null, "Brasil", Posiciones.Delantero, 1000, TraspasoJugadores.sinSolicicitar);
+        Jugadores jugador1 = new Jugadores("Vinicius", null, "Brasil", TraspasoJugadores.sinSolicicitar,
+                Posiciones.Delantero, 1000);
     }
 }
