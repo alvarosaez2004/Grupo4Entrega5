1f9384de250c783168e3df173283c6bd0ee6964c
Daniel <danielgarciaboillos1@gmail.com>
2025-01-27 14:02:10 +0100

    Warning arreglado.

--- src/com/grupo4entrega5/futbol/main/Main.java
+++ src/com/grupo4entrega5/futbol/main/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Jugadores jugador1 = new Jugadores("Vinicius", null, "Brasil", TraspasoJugadores.sinSolicicitar,
                 Posiciones.Delantero, 1000);
+        System.out.println(jugador1);
     }
 }
